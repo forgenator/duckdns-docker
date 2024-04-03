7679c220dd3ed576876bdd8e3af0ab57ee9e209c
Sofia Lindgren <forgenator@gmail.com>
2024-04-03 14:14:26 +0300

    Only do duckdns.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-DYN_HOSTNAME=${DYN_HOSTNAME:-""}
-DYN_USER=${DYN_USER:-""}
-DYN_PASS=${DYN_PASS:-""}
 DUCKDNS_HOSTNAME=${DUCKDNS_HOSTNAME:-""}
 DUCKDNS_TOKEN=${DUCKDNS_TOKEN:-""}
 WAIT_TIME=${WAIT_TIME:-"10"}
@@ -11,10 +8,6 @@ WAIT_TIME=${WAIT_TIME:-"10"}
 echo "Running..." | ts
 
 while true; do
-
-  echo -n "dynu.com: " | ts
-  curl -s "https://api.dynu.com/nice/update?username=${DYN_USER}&password=${DYN_PASS}&hostname=${DYN_HOSTNAME}"
-  echo
 
   echo -n "duckdns.org: " | ts
   curl -s "https://www.duckdns.org/update?domains=${DUCKDNS_HOSTNAME}&token=${DUCKDNS_TOKEN}"
